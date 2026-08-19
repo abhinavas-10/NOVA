68bec68a5b531d42462f26da3f17b23b91e4d63d
abhinavas-10 <abhinavasashok@gmail.com>
2026-08-19 16:36:41 +0530

    Fix NØVA encoding and product images

--- backend/products/management/commands/fix_nova_encoding.py
+++ backend/products/management/commands/fix_nova_encoding.py
@@ -6,15 +6,11 @@ class Command(BaseCommand):
     help = "Fix corrupted NØVA text encoding"
 
     def handle(self, *args, **options):
-        bad = "N\u00f1VA"
+        bad = "N\u00d1VA"
         good = "N\u00d8VA"
 
         product_count = 0
         category_count = 0
-
-        # ---------------------------------------------
-        # PRODUCTS
-        # ---------------------------------------------
 
         for product in Product.objects.all():
             changed = False
@@ -36,22 +32,15 @@ class Command(BaseCommand):
             if changed:
                 product.save()
                 product_count += 1
-
                 self.stdout.write(
                     f"Fixed product: {product.id}"
                 )
-
-        # ---------------------------------------------
-        # CATEGORIES
-        # ---------------------------------------------
 
         for category in Category.objects.all():
             changed = False
 
             if category.name and bad in category.name:
-                category.name = category.name.replace(
-                    bad, good
-                )
+                category.name = category.name.replace(bad, good)
                 changed = True
 
             if category.description and bad in category.description:
@@ -63,7 +52,6 @@ class Command(BaseCommand):
             if changed:
                 category.save()
                 category_count += 1
-
                 self.stdout.write(
                     f"Fixed category: {category.id}"
                 )
